f8080267c49d46b982beb7635803481b26740168
unknown <franocaleta123@gmail.com>
2019-01-06 21:36:38 +0100

    try catch added

--- create_df.py
+++ create_df.py
@@ -9,18 +9,23 @@ c.subscribe(['tel_buildset_fer'])
 def fun():
     return 1
 def fun2():
-    
-    first_msg = c.poll(10)
-    mapa  = first_msg.value()
-    mapa['CODE'] = region_code_for_number(phonenumbers.parse('+' + str(int(mapa['CALLEE']))))
-    callee_country = pycountry.countries.get(alpha_2=region_code_for_number(phonenumbers.parse('+' + str(int(mapa['CALLEE'])))))
-    mapa['CALLEE_COUNTRY']  = callee_country.name
-    caller_country = pycountry.countries.get(alpha_2=region_code_for_number(phonenumbers.parse('+' + str(int(mapa['CALLER'])))))
-    mapa['CALLER_COUNTRY']  = caller_country.name
+
+    while True:
+        try:
+            first_msg = c.poll(10)
+            mapa  = first_msg.value()
+            print(mapa)
+            mapa['CODE'] = region_code_for_number(phonenumbers.parse('+' + str(int(mapa['CALLEE']))))
+            callee_country = pycountry.countries.get(alpha_2=region_code_for_number(phonenumbers.parse('+' + str(int(mapa['CALLEE'])))))
+            mapa['CALLEE_COUNTRY']  = callee_country.name
+            caller_country = pycountry.countries.get(alpha_2=region_code_for_number(phonenumbers.parse('+' + str(int(mapa['CALLER'])))))
+            mapa['CALLER_COUNTRY']  = caller_country.name
 
 
-    values = [list(mapa.values())] #spremamo vrijednosti prve poruke
-    columns = list(mapa.keys()) #spremamo atribute prve poruke
-    df = pd.DataFrame(values,columns=columns)
-    return df
-#df.to_csv("dataset.csv")
+            values = [list(mapa.values())] #spremamo vrijednosti prve poruke
+            columns = list(mapa.keys()) #spremamo atribute prve poruke
+            df = pd.DataFrame(values,columns=columns)
+            return df
+        except:
+            continue
+    #df.to_csv("dataset.csv")
